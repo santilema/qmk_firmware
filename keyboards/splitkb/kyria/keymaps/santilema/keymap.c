6fc61ba92cd95abb4a25e822c0de7ff62669b24b
Santi <lema.sx@gmail.com>
2023-12-21 01:38:10 +0100

    first working version

--- keyboards/splitkb/kyria/keymaps/santilema/keymap.c
+++ keyboards/splitkb/kyria/keymaps/santilema/keymap.c
@@ -14,6 +14,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 enum layers {
     _QWERTY = 0,
@@ -25,6 +28,151 @@ enum layers {
     _ADJUST,
 };
 
+// Function prototypes
+bool get_cell_state(uint8_t x, uint8_t y);
+void update_game_of_life(void);
+void render_game_of_life(void);
+void randomize_grid(void);
+void initialize_grid(void);
+
+// Game of Life for OLED
+#define GRID_WIDTH  128
+#define GRID_HEIGHT 64
+
+// Definition of the grid
+static bool grid[GRID_HEIGHT][GRID_WIDTH];
+
+// Definition of the game of life functions
+void randomize_grid(void) {
+    for (uint8_t y = 0; y < GRID_HEIGHT; y++) {
+        for (uint8_t x = 0; x < GRID_WIDTH; x++) {
+            grid[y][x] = (random() % 2) == 0;
+        }
+    }
+}
+
+void render_game_of_life(void) {
+    oled_clear();
+
+    // iterate over each cell
+    for (uint8_t y = 0; y < GRID_HEIGHT; y++) {
+        for (uint8_t x = 0; x < GRID_WIDTH; x++) {
+            // determine the state of the cell
+            bool is_alive = get_cell_state(x, y);
+
+            if (is_alive) {
+                // if the cell is alive, draw a pixel
+                oled_write_pixel(x, y, true);
+            }
+        }
+    }
+    update_game_of_life();
+
+}
+
+bool get_cell_state(uint8_t x, uint8_t y) {
+    // Check for out-of-bounds coordinates
+    if (x >= GRID_WIDTH || y >= GRID_HEIGHT) {
+        return false;
+    }
+
+    // Return the state of the cell
+    return grid[y][x];
+}
+
+// void matrix_scan_user(void) {
+//     // Update game state
+//     update_game_of_life();
+// }
+
+void update_game_of_life(void) {
+    static bool temp_grid[GRID_HEIGHT][GRID_WIDTH];
+    // iterate over each cell
+    for (uint8_t y = 0; y < GRID_HEIGHT; y++) {
+        for (uint8_t x = 0; x < GRID_WIDTH; x++) {
+            // determine the state of the cell
+            bool is_alive = get_cell_state(x, y);
+
+            // count the number of living neighbors
+            uint8_t living_neighbors = 0;
+            for (int8_t y_offset = -1; y_offset <= 1; y_offset++) {
+                for (int8_t x_offset = -1; x_offset <= 1; x_offset++) {
+                    // skip the current cell
+                    if (x_offset == 0 && y_offset == 0) {
+                        continue;
+                    }
+
+                    // Calculate wrapped coordinates
+                    uint8_t neighbor_x = (x + x_offset + GRID_WIDTH) % GRID_WIDTH;
+                    uint8_t neighbor_y = (y + y_offset + GRID_HEIGHT) % GRID_HEIGHT;
+
+                    if (get_cell_state(neighbor_x, neighbor_y)) {
+                        living_neighbors++;
+                    }
+                }
+            }
+
+            // apply the rules of the game to temp grid
+            bool new_state = false;
+            if (is_alive) {
+                new_state = (living_neighbors == 2 || living_neighbors == 3);
+            } else {
+                new_state = (living_neighbors == 3);
+            }
+            temp_grid[y][x] = new_state;
+        }
+    }
+
+    // copy temp grid to grid
+    for (uint8_t y = 0; y < GRID_HEIGHT; y++) {
+        for (uint8_t x = 0; x < GRID_WIDTH; x++) {
+            grid[y][x] = temp_grid[y][x];
+        }
+    }
+}
+
+void initialize_grid(void) {
+    // Clear the grid first
+    for (uint8_t y = 0; y < GRID_HEIGHT; y++) {
+        for (uint8_t x = 0; x < GRID_WIDTH; x++) {
+            grid[y][x] = false;
+        }
+    }
+
+    // Set up a specific pattern (e.g., a small glider)
+    grid[30][62] = true;
+    grid[30][63] = true;
+    grid[30][64] = true;
+    grid[30][66] = true;
+    grid[31][62] = true;
+    grid[32][65] = true;
+    grid[32][66] = true;
+    grid[33][63] = true;
+    grid[33][64] = true;
+    grid[33][66] = true;
+    grid[34][62] = true;
+    grid[34][64] = true;
+    grid[34][66] = true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_RCTL:
+            if (record->event.pressed) {
+                // When key is pressed
+                initialize_grid();
+            }
+            return false;  // Skip all further processing of this key
+        case KC_RGUI:
+            if (record->event.pressed) {
+                // When key is pressed
+                randomize_grid();
+            }
+            return false;  // Skip all further processing of this key
+        default:
+            return true;   // Process all other keys normally
+    }
+}
 
 // Aliases for readability
 #define QWERTY   DF(_QWERTY)
@@ -41,6 +189,7 @@ enum layers {
 #define CTL_MINS MT(MOD_RCTL, KC_MINUS)
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
 #define SFT_CAPS  MT(MOD_LSFT, KC_CAPS)
+
 
 // Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
 // The notation `mod/tap` denotes a key that activates the modifier `mod` when held down, and
@@ -87,7 +236,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC , _______, _______, _______, _______, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
       _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
       KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP  ,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
-                                 KC_RCTL, _______, KC_RALT, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______
+                                 KC_RCTL, KC_RGUI, KC_RALT, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______
     ),
 
 /*
@@ -181,16 +330,11 @@ void keyboard_pre_init_user(void) {
   // Turn the LED off
   // (Due to technical reasons, high is off and low is on)
   writePinHigh(24);
+  initialize_grid();
 }
 
 
-/* The default OLED and rotary encoder code can be found at the bottom of qmk_firmware/keyboards/splitkb/kyria/rev1/rev1.c
- * These default settings can be overriden by your own settings in your keymap.c
- * For your convenience, here's a copy of those settings so that you can uncomment them if you wish to apply your own modifications.
- * DO NOT edit the rev1.c file; instead override the weakly defined default functions by your own.
- */
-
-
+// OLED and rotary encoder
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_180; }
 
@@ -221,6 +365,7 @@ bool oled_task_user(void) {
                 break;
             case _FUNCTION:
                 oled_write_P(PSTR("Function\n"), false);
+                initialize_grid();
                 break;
             default:
                 oled_write_P(PSTR("Undefined\n"), false);
@@ -232,19 +377,8 @@ bool oled_task_user(void) {
         oled_write_P(led_usb_state.caps_lock   ? PSTR("CAPS! ") : PSTR("       "), false);
         oled_write_P(led_usb_state.scroll_lock ? PSTR("SCRLCK ") : PSTR("       "), false);
     } else {
-        // clang-format off
-        static const char PROGMEM kyria_logo[] = {
-            0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,128,192,224,240,112,120, 56, 60, 28, 30, 14, 14, 14,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7, 14, 14, 14, 30, 28, 60, 56,120,112,240,224,192,128,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,  0,  0,  0,  0,192,224,240,124, 62, 31, 15,  7,  3,  1,128,192,224,240,120, 56, 60, 28, 30, 14, 14,  7,  7,135,231,127, 31,255,255, 31,127,231,135,  7,  7, 14, 14, 30, 28, 60, 56,120,240,224,192,128,  1,  3,  7, 15, 31, 62,124,240,224,192,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,  0,240,252,255, 31,  7,  1,  0,  0,192,240,252,254,255,247,243,177,176, 48, 48, 48, 48, 48, 48, 48,120,254,135,  1,  0,  0,255,255,  0,  0,  1,135,254,120, 48, 48, 48, 48, 48, 48, 48,176,177,243,247,255,254,252,240,192,  0,  0,  1,  7, 31,255,252,240,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,255,255,255,  0,  0,  0,  0,  0,254,255,255,  1,  1,  7, 30,120,225,129,131,131,134,134,140,140,152,152,177,183,254,248,224,255,255,224,248,254,183,177,152,152,140,140,134,134,131,131,129,225,120, 30,  7,  1,  1,255,255,254,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0,255,255,  0,  0,192,192, 48, 48,  0,  0,240,240,  0,  0,  0,  0,  0,  0,240,240,  0,  0,240,240,192,192, 48, 48, 48, 48,192,192,  0,  0, 48, 48,243,243,  0,  0,  0,  0,  0,  0, 48, 48, 48, 48, 48, 48,192,192,  0,  0,  0,  0,  0,
-            0,  0,  0,255,255,255,  0,  0,  0,  0,  0,127,255,255,128,128,224,120, 30,135,129,193,193, 97, 97, 49, 49, 25, 25,141,237,127, 31,  7,255,255,  7, 31,127,237,141, 25, 25, 49, 49, 97, 97,193,193,129,135, 30,120,224,128,128,255,255,127,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0, 63, 63,  3,  3, 12, 12, 48, 48,  0,  0,  0,  0, 51, 51, 51, 51, 51, 51, 15, 15,  0,  0, 63, 63,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 48, 48, 63, 63, 48, 48,  0,  0, 12, 12, 51, 51, 51, 51, 51, 51, 63, 63,  0,  0,  0,  0,  0,
-            0,  0,  0,  0, 15, 63,255,248,224,128,  0,  0,  3, 15, 63,127,255,239,207,141, 13, 12, 12, 12, 12, 12, 12, 12, 30,127,225,128,  0,  0,255,255,  0,  0,128,225,127, 30, 12, 12, 12, 12, 12, 12, 12, 13,141,207,239,255,127, 63, 15,  3,  0,  0,128,224,248,255, 63, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,  0,  0,  0,  0,  3,  7, 15, 62,124,248,240,224,192,128,  1,  3,  7, 15, 30, 28, 60, 56,120,112,112,224,224,225,231,254,248,255,255,248,254,231,225,224,224,112,112,120, 56, 60, 28, 30, 15,  7,  3,  1,128,192,224,240,248,124, 62, 15,  7,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-            0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  3,  7, 15, 14, 30, 28, 60, 56,120,112,112,112,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,112,112,112,120, 56, 60, 28, 30, 14, 15,  7,  3,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-        };
-        // clang-format on
-        oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
+        // Game of Life rendering for slave OLED
+        render_game_of_life();
     }
     return false;
 }
